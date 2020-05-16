bd1540d8d6c82787a9fd151a9d78b6a47f67801e
root <didguscjf95@naver.com>
2020-05-16 17:18:43 +0900

    update admin panel for some models

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,6 +5,13 @@ from . import models
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
 
-    """ Reservation Admin Definition """
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "in_progress",
+        "is_finished",
+    )
 
-    pass
+    list_filter = ("status",)
