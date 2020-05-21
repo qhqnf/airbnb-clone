64a793cdc56dbc933ec16bceeceefa503152f6ee
root <didguscjf95@naver.com>
2020-05-21 21:07:23 +0900

    create home url for practice

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse
 
-# Create your views here.
+
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
