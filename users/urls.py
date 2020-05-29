1e703211beb597e0c9a86d2316488ce73c2a6232
root <didguscjf95@naver.com>
2020-05-30 00:11:58 +0900

    change login view by class based view

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ app_name = "users"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.log_out(), name="logout"),
+    path("logout", views.log_out, name="logout"),
 ]
