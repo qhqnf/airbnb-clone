e60269fd10b849f201c44d9878a79e31b4aa3902
root <didguscjf95@naver.com>
2020-05-29 19:25:05 +0900

    create login panel

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+
+    """ LoginView Definition """
+
+    def get(self, request):
+
+        form = forms.LoginForm(initial={"email": "example@gmail.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        return render(request, "users/login.html", {"form": form})
