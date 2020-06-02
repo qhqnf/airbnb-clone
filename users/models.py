2a0db55f9027acf29b33350c3ccab22b44a5f09c
root <didguscjf95@naver.com>
2020-06-02 23:27:52 +0900

    make github login with OAuth

--- users/models.py
+++ users/models.py
@@ -34,6 +34,15 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW"),
     )
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     bio = models.TextField(default="", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
@@ -47,6 +56,9 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(
+        choices=LOGIN_CHOICES, max_length=50, default=LOGIN_EMAIL
+    )
 
     def verify_email(self):
         if self.email_verified is False:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
 from django.views.generic import FormView
@@ -59,4 +61,25 @@ def complete_verification(request, key):
     except models.User.DoesNotExist:
         # to do: add error Message
         pass
+    return redirect(reverse("core:home"))
+
+
+def github_login(request):
+    client_id = os.environ("GITHUB_ID")
+    redirect_uri = "http://127.0.0.1:8001/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
+
+
+def github_callback(request):
+    client_id = os.environ("GITHUB_ID")
+    client_secret = os.environ("GITHUB_SECRET")
+    code = request.GET.get("code", None)
+    if code is not None:
+        request = requests.POST(
+            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+            headers={"Accept": "application/json"},
+        )
+        print(request.json())
     return redirect(reverse("core:home"))
