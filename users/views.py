1935ca0dedd41f430975b3896c5ebeec6704f8d1
root <didguscjf95@naver.com>
2020-06-06 01:58:18 +0900

    update style

--- users/views.py
+++ users/views.py
@@ -6,7 +6,6 @@ from django.views.generic import FormView
 from django.contrib.auth import authenticate, login, logout
 from django.utils.encoding import escape_uri_path
 from django.core.files.base import ContentFile
-from django.contrib.auth.forms import UserCreationForm
 from . import forms, models
 
 
@@ -15,7 +14,7 @@ class LoginView(FormView):
     """ LoginView Definition """
 
     template_name = "users/login.html"
-    form_class = forms.UserCreationForm
+    form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
