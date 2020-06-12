6809df2ee38f5048c737f795857a3b603d1e9d38
root <didguscjf95@naver.com>
2020-06-12 20:08:53 +0900

    styling room detail view

--- rooms/models.py
+++ rooms/models.py
@@ -107,6 +107,10 @@ class Room(core_models.TimeStampedModel):
             return round(all_ratings / len(all_reviews), 2)
         return 0
 
-    def first_photo(self):
+    def get_first_photo(self):
         (photo,) = self.photos.all()[:1]
         return photo.file.url
+
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]
+        return photos
