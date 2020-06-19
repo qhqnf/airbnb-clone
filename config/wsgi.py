540200d0b5f40a6ff4ed03d7749626392258afe9
root <didguscjf95@naver.com>
2020-06-19 18:57:05 +0900

    aws config

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,17 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "pulsemanager.settings.production")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-if os.environ.get(
-        "DJANGO_SETTINGS_MODULE") == "pulsemanager.settings.production":
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    application = Sentry(get_wsgi_application())
-else:
-    get_wsgi_application()
+application = get_wsgi_application()
